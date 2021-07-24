2244d41c4bec1e0c3c874ae6b8fe0bf9682f6cf1
Jon Jackson <36890724+ocskier@users.noreply.github.com>
2021-07-24 09:28:15 -0400

    Update FloatBtn.js

--- client/src/components/FloatBtn.js
+++ client/src/components/FloatBtn.js
@@ -2,8 +2,6 @@ import { Button, Dropdown } from 'semantic-ui-react';
 
 const options = [
   { key: 'edit', icon: 'edit', text: 'Edit Post', value: 'edit' },
-  { key: 'delete', icon: 'delete', text: 'Remove Post', value: 'delete' },
-  { key: 'hide', icon: 'hide', text: 'Hide Post', value: 'hide' },
 ];
 
 const DropdownExampleFloating = () => (
